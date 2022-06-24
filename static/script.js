0adab0e83d1358ad9d153dc955a54e6e112db709
Kunal Kapur <kunalkapur888@gmail.com>
2022-06-24 16:15:05 -0700

    Display lyrics on front end and show if right

--- static/script.js
+++ static/script.js
@@ -1,13 +1,22 @@
 
 
-JSON.parse
+
 
 lyric_list = (info.song_lyrics);
 let count = 1
 
+const correctArtist = info.song_artist;
+const correctSongName = info.song_name;
+
 const lyricField = document.getElementById("lyric-field");
 
 const moreButton = document.getElementById("more");
+const checkButton = document.getElementById("check");
+
+const songInput = document.getElementById("song-input");
+
+
+const artistInput = document.getElementById("artist-input");
 
 moreButton.addEventListener("click", function () {
     if (count == (lyric_list).length) {
@@ -23,4 +32,25 @@ moreButton.addEventListener("click", function () {
     }
     count += 1;
 
+})
+
+checkButton.addEventListener("click", function () {
+    bothCorrect = true
+    if (songInput != correctSongName) {
+        //bothCorrect = false;
+    }
+
+    if (artistInput != correctArtist) {
+        //bothCorrect = false;
+    }
+
+    if (bothCorrect == true) {
+        let correct = document.createElement("div")
+        correct.style.color = 'green'
+        correct.style.fontSize = '40px';
+        correct.textContent = "CORRECT"
+        console.log(document.querySelector(".container h1"));
+        document.querySelector(".container .input-fields").insertBefore(correct, document.querySelectorAll(".container .input-fields" +
+            " .text-input")[1]);
+    }
 })
